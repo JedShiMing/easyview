8e1ceb7c0fd825235d43def4f0e30e21a8aa7ebe
ming.shi <ming.shi@ly.com>
2023-04-17 17:51:59 +0800

    圆角

--- lib_easyview/src/main/java/com/jed/lib/easyview/BaseEasy.java
+++ lib_easyview/src/main/java/com/jed/lib/easyview/BaseEasy.java
@@ -2,6 +2,10 @@ package com.jed.lib.easyview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -9,23 +13,35 @@ import android.view.View;
 
 /**
  * 自定义View
- * 1. 圆角, 不能切到子元素的越界,如果要控制子元素,建议用cardView
+ * 1. 圆角
  * 2. 边框
  * 3. 渐变
  * 4. 各种点击事件, 节流
  */
 public class BaseEasy {
-    private static final String TAG = "BaseEasy";
+    private static final String TAG = BaseEasy.class.getName();
 
+    private View mView;
     private GradientDrawable gradientDrawable;
+
+    private Paint paint;
+    private RectF rectF;
+    private float[] radii;
 
     private float leftTopRadius;
     private float rightTopRadius;
     private float leftBottomRadius;
     private float rightBottomRadius;
     private float radius;
+    // 是否裁剪,子组件超出父组件
+    private boolean overflow;
 
     public void initView(View view, Context context, AttributeSet attrs) {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        rectF = new RectF();
+
+        this.mView = view;
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.EasyView);
 
         radius = array.getDimension(R.styleable.EasyView_ev_radius, 0);
@@ -33,14 +49,27 @@ public class BaseEasy {
         leftBottomRadius = array.getDimension(R.styleable.EasyView_ev_left_bottom_radius, radius);
         rightTopRadius = array.getDimension(R.styleable.EasyView_ev_right_top_radius, radius);
         rightBottomRadius = array.getDimension(R.styleable.EasyView_ev_right_bottom_radius, radius);
+        overflow = array.getBoolean(R.styleable.EasyView_ev_overflow, false);
 
-        int bgColor = array.getColor(R.styleable.EasyView_ev_bg_color, 0);
-        int[] colors = new int[]{bgColor, bgColor};
-        float[] radiosColors = new float[]{leftTopRadius, leftTopRadius, rightTopRadius, rightTopRadius, rightBottomRadius, rightBottomRadius, leftBottomRadius, leftBottomRadius};
-        gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
-        gradientDrawable.setCornerRadii(radiosColors);
+        radii = new float[]{leftTopRadius, leftTopRadius, rightTopRadius, rightTopRadius, rightBottomRadius, rightBottomRadius, leftBottomRadius, leftBottomRadius};
 
-        view.setBackground(gradientDrawable);
+        if (!overflow) {
+            int bgColor = array.getColor(R.styleable.EasyView_ev_bg_color, 0);
+            int[] colors = new int[]{bgColor, bgColor};
+            gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, colors);
+            gradientDrawable.setCornerRadii(radii);
+            view.setBackground(gradientDrawable);
+        }
+
         array.recycle();
+    }
+
+    public void draw(Canvas canvas) {
+        if (overflow) {
+            rectF.set(0, 0, mView.getWidth(), mView.getHeight());
+            Path path = new Path();
+            path.addRoundRect(rectF, radii, Path.Direction.CW);
+            canvas.clipPath(path);
+        }
     }
 }
--- lib_easyview/src/main/java/com/jed/lib/easyview/EasyImageView.java
+++ lib_easyview/src/main/java/com/jed/lib/easyview/EasyImageView.java
@@ -1,7 +1,13 @@
 package com.jed.lib.easyview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,13 +15,19 @@ import androidx.appcompat.widget.AppCompatImageView;
 
 public class EasyImageView extends AppCompatImageView {
     private BaseEasy customView = new BaseEasy();
+
     public EasyImageView(@NonNull Context context) {
         this(context, null);
     }
 
     public EasyImageView(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
         customView.initView(this, context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        customView.draw(canvas);
+        super.onDraw(canvas);
     }
 }
