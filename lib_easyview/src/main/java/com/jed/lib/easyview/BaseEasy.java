0316ffdf31f88c0e69120f3c31bcba6f27ee31db
ming.shi <ming.shi@ly.com>
2023-04-17 18:17:46 +0800

    边框30%

--- lib_easyview/src/main/java/com/jed/lib/easyview/BaseEasy.java
+++ lib_easyview/src/main/java/com/jed/lib/easyview/BaseEasy.java
@@ -3,6 +3,7 @@ package com.jed.lib.easyview;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
@@ -28,17 +29,23 @@ public class BaseEasy {
     private RectF rectF;
     private float[] radii;
 
+    // 圆角
     private float leftTopRadius;
     private float rightTopRadius;
     private float leftBottomRadius;
     private float rightBottomRadius;
     private float radius;
-    // 是否裁剪,子组件超出父组件
+    // 是否裁剪,子组件超出父组件的时候需要
     private boolean overflow;
+    // 边框
+    private int strokeColor;
+    private float strokeWidth;
 
     public void initView(View view, Context context, AttributeSet attrs) {
         paint = new Paint();
+        paint = new Paint();
         paint.setAntiAlias(true);
+        rectF = new RectF();
         rectF = new RectF();
 
         this.mView = view;
@@ -49,6 +56,9 @@ public class BaseEasy {
         leftBottomRadius = array.getDimension(R.styleable.EasyView_ev_left_bottom_radius, radius);
         rightTopRadius = array.getDimension(R.styleable.EasyView_ev_right_top_radius, radius);
         rightBottomRadius = array.getDimension(R.styleable.EasyView_ev_right_bottom_radius, radius);
+        strokeColor = array.getColor(R.styleable.EasyButton_ev_stroke_color, Color.TRANSPARENT);
+        strokeWidth = array.getDimension(R.styleable.EasyButton_ev_stroke_width, 0f);
+
         overflow = array.getBoolean(R.styleable.EasyView_ev_overflow, false);
 
         radii = new float[]{leftTopRadius, leftTopRadius, rightTopRadius, rightTopRadius, rightBottomRadius, rightBottomRadius, leftBottomRadius, leftBottomRadius};
@@ -71,5 +81,18 @@ public class BaseEasy {
             path.addRoundRect(rectF, radii, Path.Direction.CW);
             canvas.clipPath(path);
         }
+        drawBorder(canvas);
     }
+
+    private void drawBorder(Canvas canvas) {
+        if (strokeWidth > 0f && strokeColor != Color.TRANSPARENT) {
+            paint.setColor(strokeColor);
+            paint.setStrokeWidth(strokeWidth);
+            paint.setStyle(Paint.Style.STROKE);
+            Path path = new Path();
+            path.addRoundRect(rectF, radii, Path.Direction.CW);
+            canvas.drawPath(path, paint);
+        }
+    }
+
 }
